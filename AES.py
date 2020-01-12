6396927d4e2f525dd09e6d9a029ed1538d0c9add
YaChaMi <43241465+YaChaMi@users.noreply.github.com>
2020-01-12 17:50:04 +0800

    Add the file of Cryptography
    encryption and decryption by AES

---
+++ AES.py
@@ -0,0 +1,20 @@
+from Cryptodome.Cipher import AES
+from binascii import b2a_hex, a2b_hex
+
+def pad(string): # pad ' ' to string
+    while len(string) % 16 != 0:
+        string += ' '
+    return string
+
+key = 'CNFinalProject' # private key
+aes = AES.new(pad(key).encode(), AES.MODE_ECB) # construct AES by private key
+
+def encrypt(plaintext): # encryption of AES
+    cyphertext_byte = aes.encrypt(pad(plaintext).encode())
+    cyphertext_hex = b2a_hex(cyphertext_byte)
+    cyphertext = cyphertext_hex.decode()
+    return cyphertext
+
+def decrypt(cyphertext): # decryption of AES
+    plaintext = str(aes.decrypt(a2b_hex(pad(cyphertext).encode())), encoding='utf-8',errors="ignore")
+    return plaintext
